0a4a26955785d75dd8cd6b5f761ff6548e753c9a
Sosthène Guédon <sosthene@nitrokey.com>
2022-11-17 16:09:03 +0100

    Fix key length

--- src/config.rs
+++ src/config.rs
@@ -48,7 +48,7 @@ pub const MAX_SHORT_DATA_LENGTH: usize = 128;
 pub const MAX_SIGNATURE_LENGTH: usize = 512 * 2;
 #[cfg(any(feature = "rsa2048", feature = "rsa3072", feature = "rsa4096"))]
 // FIXME: Value from https://stackoverflow.com/questions/5403808/private-key-length-bytes for Rsa2048 Private key
-pub const MAX_KEY_MATERIAL_LENGTH: usize = 1232 * 2;
+pub const MAX_KEY_MATERIAL_LENGTH: usize = 1160 * 2 + 72;
 #[cfg(any(feature = "rsa2048", feature = "rsa3072", feature = "rsa4096"))]
 // This is due to the fact that KEY_MATERIAL_LENGTH is bigger than MESSAGE_LENGTH for RSA.
 pub const MAX_MESSAGE_LENGTH: usize = 1024;
